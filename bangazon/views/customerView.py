ae87189cf41d066f37827f6fcb2fd844ecc5a8df
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-25 13:47:49 -0600

    added active=false and active=true queries

--- bangazon/views/customerView.py
+++ bangazon/views/customerView.py
@@ -2,13 +2,12 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
-
 from rest_framework import filters
 
-from bangazon.models import Customer, Order
-from bangazon.serializers import ProductSerializer, CustomerSerializer
+from bangazon.models import Customer
+from bangazon.models import Order
+from bangazon.serializers import ProductSerializer
+from bangazon.serializers import CustomerSerializer
 
 
 @api_view(['GET'])
@@ -19,64 +18,39 @@ def api_root(requst, format=None):
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
-#   print("QUERY: ", queryset)
     serializer_class = CustomerSerializer
-    # filter_backends = (filters.SearchFilter, )
-    # search_fields = ('id', 'firstName', 'lastName', 'email', 'address', 'phone', 'deletedOn', 'url')
-
 
     def get_queryset(self):
-        # print("SELF: ", )
         keyword = self.request.query_params.get('active', None)
         customer_query_set = Customer.objects.all()
         print("CUSTOMER QUERY: ", customer_query_set.values())
         order_query_set = Order.objects.all()
 
         if keyword == "false":
-            # customerList = []
-            notCustomerList = []
-            idList = []
+            notCustomer = Customer.objects.filter(active=False)
 
-            # notCustomer = Customer.objects.
+            return notCustomer
 
-            for order in order_query_set:
-                buyer_id = order.buyer_id
-                # print("BUYER ID: ", buyer_id)
-                # customer = customer_query_set.filter(id=buyer_id)
-                customer = Customer.objects.all().exclude(id=buyer_id).get(id=9)
-                print("CUSTOMER: ", customer)
-                # customerList.append(customer)
-                customerList = [customer]
-
-            # for customer in customerList:
-            #     customer_id = customer.id
-            #     idList.append(customer_id)
-            #     print("CUSTOMER ID: ", customer_id)
-            #     # notCustomers = Customer.objects.filter(~Q(id = customer_id))
-            #     notCustomers = Customer.objects.exclude(id=customer_id)
-            #     print("NOT CUSTOMERS: ", notCustomers, "END OF LIST")
-            #     notCustomerList.append(notCustomers)
-
-            # print("CUSTOMER LIST: ", customerList, "END OF LIST")
-            # print("NOT CUSTOMER LIST: ", notCustomerList, "END OF LIST")
-            return customerList
-
-
-# THIS ONE IS WORKING
-        if keyword is not None:
+        elif keyword == "true":
             customerList = []
 
 
             for order in order_query_set:
                 buyer_id = order.buyer_id
-                # print("BUYER ID: ", buyer_id)
-                # customer = customer_query_set.filter(id=buyer_id)
                 customer = Customer.objects.get(id=buyer_id)
                 customerList.append(customer)
 
+            return customerList
+
+        elif keyword is not None:
+            customerList = []
 
 
-            # print("CUSTOMER LIST: ", customerList)
+            for order in order_query_set:
+                buyer_id = order.buyer_id
+                customer = Customer.objects.get(id=buyer_id)
+                customerList.append(customer)
+
             return customerList
 
 
